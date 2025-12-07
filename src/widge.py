0da88c84f980682adea5c00ec73e71a06cb23ba5
Serega Ivkin <kroll-87@mail.ru>
2025-12-07 11:34:03 +0300

    add widge.py with mask_account_card

---
+++ src/widge.py
@@ -0,0 +1,22 @@
+def mask_account_card(info_string: str) -> str:
+    """    Маскирует номер карты или счета в строке.    """
+    parts = info_string.split()
+    if not parts:
+        return ""
+    account_type = " ".join(parts[:-1])
+    number = parts[-1]
+    if account_type.lower() == "счет":
+        if len(number) >= 4:
+            masked_number = "**" + number[-4:]
+        else:
+            masked_number = number
+        return f"{account_type} {masked_number}"
+    else:
+        if len(number) == 16 and number.isdigit():
+            masked_number = f"{number[:4]} {number[4:6]}** **** {number[-4:]}"
+        else:
+            masked_number = number
+        return f"{account_type} {masked_number}"
+
+
+
